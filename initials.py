1554869d9ae89aa8d0f71cd99bb7cba51719f6e6
AndyObeng1122 <percent034@gmail.com>
2026-05-31 17:56:00 -0400

    Implement function to display initials
    Added a function to print initials in stylized letters.

--- initials.py
+++ initials.py
@@ -1,6 +1,37 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Obeng Andy Akwesi Amponsah
+# STUDENT ID: 2425403658
+# Email: obengandy122@gmail.com
+# Grader: Andy
+#
+# On my honor, Obeng Andy Akwesi Amponsah, this programming
+# assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+
+    print()
+
+    print("...OAA")
+
+    print()
+
+    print("...OOOOOOOOOOOO........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print("...OO........OO........AA........AA........AA........AA.....")
+    print("...OO........OO........AA........AA........AA........AA.....")
+    print("...OO........OO........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print("...OO........OO........AA........AA........AA........AA.....")
+    print("...OO........OO........AA........AA........AA........AA.....")
+    print("...OO........OO........AA........AA........AA........AA.....")
+    print("...OO........OO........AA........AA........AA........AA.....")
+    print("...OO........OO...**...AA........AA...**...AA........AA...**")
+    print("...OOOOOOOOOOOO...**...AA........AA...**...AA........AA...**")
+
+    print()
 
 
 
